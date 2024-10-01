e49e7d35d565e33d4c30e2144cdf9a528a2e0fe0
Bedroom123321 <romanbadest@gmail.com>
2024-10-01 23:37:49 +0300

    LAB1 clion2

---
+++ LAB1/header_files/functions.h
@@ -0,0 +1,15 @@
+#include "list_of_visitors_class.h"
+
+using std::cout;
+using std::cin;
+using std::string;
+
+void print_menu();
+
+List_of_visitors case1_enter(List_of_visitors list);
+
+void case2_show(List_of_visitors const& list);
+
+List_of_visitors case3_change(List_of_visitors list);
+
+List_of_visitors case4_delete(List_of_visitors list);
