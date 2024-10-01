a73c621d84e0763503f562cb8836726b0c690f70
Bedroom123321 <romanbadest@gmail.com>
2024-10-01 23:31:01 +0300

    LAB1 Clion

---
+++ LAB1/cpp_files/visitor_class.cpp
@@ -0,0 +1,19 @@
+#include "../header_files/visitor_class.h"
+
+std::string Visitor::get_name() const
+{
+    return name;
+}
+
+Visitor::Visitor(const string& vis_name):name(vis_name){}
+
+void Visitor :: set_name(const string& vis_name)
+{
+    name = vis_name;
+}
+
+void Visitor:: print_name() const
+{
+    std::cout << get_name();
+}
+
---
+++ LAB1/header_files/list_of_visitors_class.h
@@ -0,0 +1,27 @@
+#ifndef LIST_OF_VISITORS_H
+#define LIST_OF_VISITORS_H
+#include <vector>
+#include "visitor_class.h"
+
+using std::string;
+using std::vector;
+
+class List_of_visitors
+{
+private:
+
+    vector<Visitor> visitors;
+
+public:
+
+    void add_data(const string& v_name);
+
+    void print_data(const int& number) const;
+
+    void chang_data(const int& number,const string& new_name);
+
+    void delete_data(const int& number);
+
+    size_t size_visitors() const;
+};
+#endif
